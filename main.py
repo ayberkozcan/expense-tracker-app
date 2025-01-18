9c412cc8e846c084ca18c2d6cbf35a744fc568ec
ayberkozcan <ozcanayberk249@gmail.com>
2025-01-18 23:37:52 +0300

    Add income / expense pages design

--- main.py
+++ main.py
@@ -110,9 +110,76 @@ class ExpenseTracker(ctk.CTk):
         favourite_income_label = ctk.CTkLabel(self.content_frame, text="Favourite Income Category").grid(row=2, column=0)
         favourite_expense_label = ctk.CTkLabel(self.content_frame, text="Favourite Expense Category").grid(row=2, column=1)
 
-        add_income_button = ctk.CTkButton(self.content_frame, text="Add Income", font=("Helvetica", 20), height=50, fg_color="#008f11", hover_color="#00690c", corner_radius=20).grid(row=3, column=0)
-        add_expense_button = ctk.CTkButton(self.content_frame, text="Add Expense", font=("Helvetica", 20), height=50, fg_color="#a71e00", hover_color="#7d1600", corner_radius=20).grid(row=3, column=1)
+        add_income_button = ctk.CTkButton(self.content_frame, text="Add Income", font=("Helvetica", 20), height=50, fg_color="#008f11", hover_color="#00690c", corner_radius=20, command=lambda: self.add_income_expense_screen("income")).grid(row=3, column=0)
+        add_expense_button = ctk.CTkButton(self.content_frame, text="Add Expense", font=("Helvetica", 20), height=50, fg_color="#a71e00", hover_color="#7d1600", corner_radius=20, command=lambda: self.add_income_expense_screen("expense")).grid(row=3, column=1)
     
+    def add_income_expense_screen(self, operation):
+        self.clear_content_frame()
+        
+        if operation == "income":
+            text = "Add Income"
+        else:
+            text = "Add Expense"
+        self.header.configure(text=text)
+
+        # Center frame
+        center_frame = ctk.CTkFrame(self.content_frame, fg_color="transparent")
+        center_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=100)
+
+        center_frame.grid_columnconfigure(0, weight=1)
+        center_frame.grid_columnconfigure(1, weight=1)
+
+        # Amount field
+        label = ctk.CTkLabel(center_frame, text="Amount", font=("Helvetica", 20)).grid(row=0, column=0, sticky="w")
+        
+        validate_command = (self.content_frame.register(self.validate_number), "%P")
+
+        amount_entry = ctk.CTkEntry(center_frame, placeholder_text="$", corner_radius=5, height=25, width=250, validate="key", validatecommand=validate_command)
+        amount_entry.grid(row=1, column=0, columnspan=2, sticky="w", pady=5)
+
+        # Category field
+        if operation == "income":
+            categories = ["Other", "Salary", "Freelance", "Investment", "Rental Income", "Gift"]
+        else:
+            categories = ["Other", "Housing", "Food", "Transportation", "Healthcare", "Entertainment"]
+        
+        selected_category = ctk.StringVar(value=categories[0])
+
+        label = ctk.CTkLabel(center_frame, text="Category", font=("Helvetica", 20)).grid(row=2, column=0, sticky="w", pady=(25, 0))
+        category_selectBox = ctk.CTkOptionMenu(center_frame, values=categories, variable=selected_category)
+        category_selectBox.grid(row=3, column=0, columnspan=2, sticky="w", pady=5)
+
+        # Note field
+        label = ctk.CTkLabel(center_frame, text="Note", font=("Helvetica", 20)).grid(row=4, column=0, sticky="w", padx=10, pady=(25, 0))
+        note_textBox = ctk.CTkTextbox(center_frame, corner_radius=5, height=50, width=250)
+        note_textBox.grid(row=5, column=0, columnspan=2, sticky="w", pady=5)
+
+        # Submit button
+        submit_button = ctk.CTkButton(center_frame, text="Submit", font=("Helvetica", 20), corner_radius=5, height=35, width=150, command=self.dashboard_page)
+        submit_button.grid(row=6, column=0, columnspan=2, sticky="w", pady=(25, 0))
+
+        # Go Back button
+        button = ctk.CTkButton(center_frame, text="Go Back", font=("Helvetica", 20), corner_radius=5, height=35, width=150, command=self.dashboard_page)
+        button.grid(row=7, column=0, columnspan=2, sticky="w", pady=(5, 20))
+
+        def focus_selectbox(event):
+            category_selectBox.focus_set()
+
+        def focus_textBox(event):
+            note_textBox.focus_set()
+
+        def on_submit(event):
+            submit_button.focus_set()
+
+        amount_entry.bind("<Return>", focus_selectbox)
+        category_selectBox.bind("<Return>", focus_textBox)
+        note_textBox.bind("<Return>", on_submit)
+
+    def validate_number(self, input_value):
+        if input_value == "" or input_value.isdigit():
+            return True
+        return False
+
     def exit(self):
         self.destroy()
 

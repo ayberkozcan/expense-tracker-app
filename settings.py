ead950a7e5a1c5345a96d31c08c2916b58a0c8b1
ayberkozcan <ozcanayberk249@gmail.com>
2025-01-20 22:45:55 +0300

    Category screen design

--- settings.py
+++ settings.py
@@ -51,9 +51,12 @@ def settings_page(self):
         button = ctk.CTkButton(theme_color_frame, text=color["text"], width=50, height=40, corner_radius=10, font=("Helvetica", 14), command=lambda color=color: set_color(self, color["color"]))
         button.grid(row=3, column=i, padx=(0, 10), sticky="w")
     
+    categories_button = ctk.CTkButton(self.content_frame, text="Manage Categories", height=40, command=lambda: manage_categories_screen(self))
+    categories_button.grid(row=2, column=0, padx=20, columnspan=2, sticky="w")
+
     # Languages Frame
     languages_frame = ctk.CTkFrame(self.content_frame, fg_color="transparent")
-    languages_frame.grid(row=2, column=0, padx=20, columnspan=2, sticky="nsew")
+    languages_frame.grid(row=3, column=0, padx=20, columnspan=2, sticky="nsew")
 
     label = ctk.CTkLabel(languages_frame, text="Language", font=("Helvetica", 20))
     label.grid(row=0, column=0, pady=10, sticky="w")
@@ -62,7 +65,7 @@ def settings_page(self):
 
     for i, language in enumerate(languages):
         button = ctk.CTkButton(languages_frame, text=language, fg_color="transparent", hover=None, font=("Helvetica", 14))
-        button.grid(row=1, column=i, sticky="w")
+        # button.grid(row=1, column=i, sticky="w")
 
     # Delete Data
     delete_data = ctk.CTkButton(
@@ -74,12 +77,12 @@ def settings_page(self):
         font=("Helvetica", 20),
         corner_radius=10
     )
-    delete_data.grid(row=3, column=0, padx=20, pady=10, sticky="w")
+    delete_data.grid(row=4, column=0, padx=20, pady=(10, 20), sticky="w")
 
 def change_currency(self, temp_currency):
     response = messagebox.askyesno(
         "Warning",
-        "Are you sure you want to change currency?"
+        f"Are you sure you want to change currency to {temp_currency}?"
     )
     if response:
         self.currency = temp_currency
@@ -104,3 +107,35 @@ def set_color(self, color):
     settings_page(self)
 
     # Save to file
+
+def create_category_frame(self, category_type, categories, row, column):
+    category_label = ctk.CTkLabel(self.content_frame, text=f"{category_type}\nCategories", font=("Helvetica", 25))
+    category_label.grid(row=row, column=column)
+
+    category_frame = ctk.CTkFrame(self.content_frame, fg_color="transparent")
+    category_frame.grid(row=row + 1, column=column, padx=40, pady=20, sticky="nsew")
+    category_frame.grid_columnconfigure(0, weight=8)
+    category_frame.grid_columnconfigure(1, weight=1)
+    category_frame.grid_columnconfigure(2, weight=1)
+
+    for i, category in enumerate(categories):
+        label = ctk.CTkLabel(category_frame, text=category, font=("Helvetica", 15))
+        label.grid(row=i, column=0, pady=10, sticky="w")
+        edit_button = ctk.CTkButton(category_frame, text="Edit", width=20, fg_color="green", hover_color="darkgreen")
+        edit_button.grid(row=i, column=1, sticky="e")
+        delete_button = ctk.CTkButton(category_frame, text="Delete", width=20, fg_color="red", hover_color="darkred")
+        delete_button.grid(row=i, column=2, sticky="e")
+
+def manage_categories_screen(self):
+    self.clear_content_frame()
+    self.header.configure(text="Manage Categories")
+    
+    create_category_frame(self, "Income", self.income_categories, 0, 0)
+    
+    create_category_frame(self, "Expense", self.expense_categories, 0, 1)
+
+    button1 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen")
+    button1.grid(row=2, column=0)
+
+    button2 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen")
+    button2.grid(row=2, column=1)

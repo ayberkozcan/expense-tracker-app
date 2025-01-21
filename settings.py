1ecc421836408871dd16e546001a967aaadb8b5e
ayberkozcan <ozcanayberk249@gmail.com>
2025-01-21 14:44:41 +0300

    Connecting, adding and deleting categories

--- settings.py
+++ settings.py
@@ -118,15 +118,24 @@ def create_category_frame(self, category_type, categories, row, column):
     category_frame.grid_columnconfigure(1, weight=1)
     category_frame.grid_columnconfigure(2, weight=1)
 
+    if not categories:
+        category_frame.grid_columnconfigure(0, weight=1)
+        category_frame.grid_columnconfigure(1, weight=1)
+        category_frame.grid_columnconfigure(2, weight=1)
+        label = ctk.CTkLabel(category_frame, text="No data found", font=("Helvetica", 15))
+        label.grid(row=0, column=1, pady=10, sticky="")
+        
     for i, category in enumerate(categories):
         label = ctk.CTkLabel(category_frame, text=category, font=("Helvetica", 15))
         label.grid(row=i, column=0, pady=10, sticky="w")
-        edit_button = ctk.CTkButton(category_frame, text="Edit", width=20, fg_color="green", hover_color="darkgreen")
+        edit_button = ctk.CTkButton(category_frame, text="Edit", width=20, fg_color="green", hover_color="darkgreen", command=lambda category_name=category[0]: edit_category(self, category_name))
         edit_button.grid(row=i, column=1, sticky="e")
-        delete_button = ctk.CTkButton(category_frame, text="Delete", width=20, fg_color="red", hover_color="darkred")
+        delete_button = ctk.CTkButton(category_frame, text="Delete", width=20, fg_color="red", hover_color="darkred", command=lambda category_name=category[0]: delete_category(self, category_name))
         delete_button.grid(row=i, column=2, sticky="e")
 
 def manage_categories_screen(self):
+    self.get_categories()
+
     self.clear_content_frame()
     self.header.configure(text="Manage Categories")
     
@@ -134,8 +143,72 @@ def manage_categories_screen(self):
     
     create_category_frame(self, "Expense", self.expense_categories, 0, 1)
 
-    button1 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen")
+    button1 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen", command=lambda: add_category(self, "income"))
     button1.grid(row=2, column=0)
 
-    button2 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen")
+    button2 = ctk.CTkButton(self.content_frame, text="Add Category", height=40, corner_radius=5, fg_color="green", hover_color="darkgreen", command=lambda: add_category(self, "expense"))
     button2.grid(row=2, column=1)
+
+def edit_category(self, category_id):
+    edit_screen = ctk.CTkToplevel(self)
+    edit_screen.geometry("300x150")
+    edit_screen.title("Edit Category")
+
+def delete_category(self, category_name):
+    confirm = messagebox.askyesno(
+        "Delete Category",
+        "Are you sure you want to delete?"
+    )
+    if confirm:
+        self.cursor.execute("DELETE FROM categories WHERE name = ?", (category_name,))
+        self.conn.commit()
+        messagebox.showinfo("Success", "Category deleted successfully!")
+        manage_categories_screen(self)
+    
+def add_category(self, category_type):
+    if len(self.income_categories) > 9 and category_type == "income":
+        messagebox.showerror("Error", "You cannot add more income categories!")
+        return
+    if len(self.expense_categories) > 9 and category_type == "expense":
+        messagebox.showerror("Error", "You cannot add more expense categories!")
+        return
+
+    def limit_input(*args):
+        current_text = entry_var.get()
+        if len(current_text) > 20:
+            entry_var.set(current_text[:20])
+
+    self.add_category_screen = ctk.CTkToplevel(self)
+    self.add_category_screen.geometry("250x200")
+    self.add_category_screen.title("Add Category")
+
+    self.add_category_screen.grid_rowconfigure(0, weight=1)
+    self.add_category_screen.grid_rowconfigure(1, weight=1)
+    self.add_category_screen.grid_columnconfigure(0, weight=1)
+
+    entry_var = ctk.StringVar()
+    entry_var.trace("w", limit_input)
+
+    entry = ctk.CTkEntry(self.add_category_screen, textvariable=entry_var, placeholder_text="Category Name...", corner_radius=5)
+    entry.grid(row=0, column=0, pady=20)
+
+    submit = ctk.CTkButton(self.add_category_screen, text="Add", fg_color="green", hover_color="darkgreen", command=lambda: add_category_to_db(self, entry.get(), category_type))
+    submit.grid(row=1, column=0, pady=20)
+
+def add_category_to_db(self, name, category_type):
+    if len(name) == 0:
+        messagebox.showerror("Error", "Category name must be longer!")
+    else:
+        control = self.cursor.execute("SELECT * FROM categories WHERE name = :entry", {"entry": name}).fetchall()
+        if control:
+            messagebox.showerror("Error", "This category already exists!")
+        else:
+            self.cursor.execute("""INSERT INTO categories (name, type) VALUES(:name, :type)""", {"name": name, "type": category_type})
+            self.conn.commit()
+            messagebox.showinfo("Success", "New category added successfully!")
+            
+            self.add_category_screen.destroy()
+            manage_categories_screen(self)
+            
+
+    

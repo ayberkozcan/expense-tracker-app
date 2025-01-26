686ebee0241685f5cf0924f504cdb1211fe436bb
ayberkozcan <ozcanayberk249@gmail.com>
2025-01-26 19:38:41 +0300

    Showing latest incomes

--- incomes.py
+++ incomes.py
@@ -18,8 +18,7 @@ def incomes_page(self):
     # Top Frame
     top_frame = ctk.CTkFrame(self.content_frame, fg_color="transparent")
     top_frame.grid(row=1, column=0, columnspan=2, padx=20, sticky="nsew")
-    top_frame.grid_columnconfigure(0, weight=1)
-    top_frame.grid_columnconfigure(1, weight=1)
+    top_frame.grid_columnconfigure((0, 1), weight=1)
 
     # Chart Frame
     chart_frame = ctk.CTkFrame(top_frame, fg_color="transparent")
@@ -41,17 +40,21 @@ def incomes_page(self):
     canvas.get_tk_widget().grid(row=1, column=0, pady=(10, 5), columnspan=4, sticky="nsew")
     
     # Summary Frame
-    summary_frame = ctk.CTkScrollableFrame(top_frame, fg_color="transparent")
+    summary_frame = ctk.CTkFrame(top_frame, fg_color="transparent")
     summary_frame.grid(row=0, column=1, sticky="nsew")
+    summary_frame.grid_columnconfigure((0, 1, 2), weight=1)
 
-    summary_header = ctk.CTkLabel(summary_frame, text="Summary (Last 10)", font=("Helvetica", 20)).grid(row=0, column=0, pady=(5, 20), sticky="w")
+    summary_header = ctk.CTkLabel(summary_frame, text="Latest Incomes", font=("Helvetica", 20)).grid(row=0, column=0, pady=(5, 20), sticky="w")
     
-    # Last 10 Records in Summary Frame
-    money = 0 # Delete later
-    for i in range(10):
-        money += 100
-        label = ctk.CTkLabel(summary_frame, text="*"+str(money).join(" $")+" (Category)").grid(row=i+1, column=0, pady=5, sticky="w")
-        details = ctk.CTkButton(summary_frame, text="Details", width=20).grid(row=i+1, column=1)
+    # Last Records in Summary Frame
+    label = ctk.CTkLabel(summary_frame, text="Amount").grid(row=1, column=0, pady=5, sticky="w")
+    label = ctk.CTkLabel(summary_frame, text="Category").grid(row=1, column=1, pady=5, sticky="w")
+    incomes = self.get_latest_transactions("income", 6)
+    for i, amount in enumerate(incomes):
+        label = ctk.CTkLabel(summary_frame, text=f"{incomes[i][0]} $").grid(row=i+2, column=0, pady=5, sticky="w")
+        category = ctk.CTkLabel(summary_frame, text=incomes[i][1]).grid(row=i+2, column=1, sticky="w")
+        details = ctk.CTkButton(summary_frame, text="Details", width=20)
+        details.grid(row=i+2, column=2, sticky="")
 
     # Period Buttons
     this_week_button = ctk.CTkButton(chart_frame, text="This Week", width=40).grid(row=0, column=0, sticky="w")
@@ -62,23 +65,30 @@ def incomes_page(self):
     # Footer Frame
     footer_frame = ctk.CTkFrame(self.content_frame, fg_color="transparent")
     footer_frame.grid(row=2, column=0, columnspan=2, padx=20, pady=10, sticky="nsew")
-    footer_frame.grid_columnconfigure(0, weight=1)
-    footer_frame.grid_columnconfigure(1, weight=1)
+    footer_frame.grid_columnconfigure((0, 1), weight=1)
+    
+    categories = []
+    for i, category in enumerate(self.income_categories):
+        categories.append(category[0])
+    
+    selected_category = ctk.StringVar(value=categories[0])
 
-    self.income_categories = ["All Categories", "Salary", "Freelance", "Investment", "Rental Income", "Gift"]
-    selected_category = ctk.StringVar(value=self.income_categories[0])
-
-    categories_selectBox = ctk.CTkOptionMenu(footer_frame, values=self.income_categories, variable=selected_category).grid(row=0, column=0, pady=5, sticky="w")
+    categories_selectBox = ctk.CTkOptionMenu(footer_frame, values=categories, variable=selected_category)
+    categories_selectBox.grid(row=0, column=0, pady=5, sticky="w")
 
     # Left Footer Frame
     records_by_categories_frame = ctk.CTkScrollableFrame(footer_frame, fg_color="transparent")
     records_by_categories_frame.grid(row=1, column=0, sticky="nsew")
+    records_by_categories_frame.grid_columnconfigure((0, 1), weight=1)
 
-    money = 0 # Delete later
-    for i in range(5):
-        money += 100
-        label = ctk.CTkLabel(records_by_categories_frame, text="*"+str(money).join(" $")).grid(row=i, column=0, pady=5, sticky="w")
-        details = ctk.CTkButton(records_by_categories_frame, text="Details", width=20).grid(row=i, column=1, padx=10)
+    incomes = self.get_latest_transactions_by_category("income", "Other", 10)
+
+    for i, amount in enumerate(incomes):
+        label = ctk.CTkLabel(records_by_categories_frame, text=f"{incomes[i][0]} $").grid(row=i+2, column=0, pady=5, sticky="w")
+        details = ctk.CTkButton(records_by_categories_frame, text="Details", width=20)
+        details.grid(row=i+2, column=1, sticky="")
+        delete = ctk.CTkButton(records_by_categories_frame, text="Delete", width=20, fg_color="red", hover_color="darkred")
+        delete.grid(row=i+2, column=1, sticky="e")
 
     label = ctk.CTkLabel(footer_frame, text="Total Income by Categories "+"This Week", font=("Helvetica", 15)).grid(row=0, column=1, sticky="w")
 
@@ -86,5 +96,5 @@ def incomes_page(self):
     income_by_categories_frame = ctk.CTkScrollableFrame(footer_frame, fg_color="transparent")
     income_by_categories_frame.grid(row=1, column=1, sticky="nsew")
 
-    for i, category in enumerate(self.income_categories, start=1):
+    for i, category in enumerate(categories, start=1):
         label = ctk.CTkLabel(income_by_categories_frame, text="* "+category).grid(row=i, column=0, pady=5, sticky="w")
